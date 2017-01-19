9c25f5819ee2ada1305e3427677244cded82c396
daniel <dgr7348@students.fhv.at>
2017-01-19 15:56:46 +0100

    png

--- Uebung01/ReportEx3.m
+++ Uebung01/ReportEx3.m
@@ -3,10 +3,9 @@
 
 figure("1");
 
-yStart = ones(100, 1);
+yStart = ones(10, 1)*10;
 sigma = 1;
-sigmaStop = 10^(-5);
-randn('state',7);
+sigmaStop = 10^(-2);
 [fitnessHistoryOneFifth, fitnessParentOneFifth, generationCountOneFifth, sigmaHistoryOneFifth, yParentOneFifth] = OnePlusOneESOneFifth("Sphere", [], yStart, sigma, sigmaStop);
 
 set (0, "defaultaxesfontname", "Helvetica")
@@ -22,13 +21,22 @@ semilogy(sigmaHistoryOneFifth,'g-');
 
 hold on
 
-maxGenCount = 5000;
+maxGenCount = 1700;
 result2 = OnePlusOneES(@Sphere, yStart, sigma, maxGenCount);
 semilogy(result2,'r-');
 
-legend("1+1 and 1/5","1+1", "Sigma");
+legend("1+1 and 1/5", "Sigma", "1+1");
 
 ylabel("Fitness / Sigma Value");
 xlabel("Generation Number");
 
 title("Exercise 3 - Fitness - 1+1 ES");
+
+% check result
+yStart = ones(100, 1);
+sigma = 1;
+sigmaStop = 10^(-5);
+randn('state',7);
+[fitnessHistory, fitnessParent, generationCount, sigmaHistory, yParent] = OnePlusOneESOneFifth("Sphere", [], yStart, sigma, sigmaStop);
+fitnessParent
+generationCount
