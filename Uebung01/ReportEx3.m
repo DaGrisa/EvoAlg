f3f1009191edd65332668c6526177d88418aec69
daniel <dgr7348@students.fhv.at>
2017-01-19 13:22:47 +0100

    pdf results

--- Uebung01/ReportEx3.m
+++ Uebung01/ReportEx3.m
@@ -1,29 +1,34 @@
 % Aufgabe 3 %
 %%%%%%%%%%%%%
 
-figure("0");
+figure("1");
 
 yStart = ones(100, 1);
 sigma = 1;
 sigmaStop = 10^(-5);
 randn('state',7);
-result = OnePlusOneESOneFifth("Sphere", [], yStart, sigma, sigmaStop);
+[fitnessHistoryOneFifth, fitnessParentOneFifth, generationCountOneFifth, sigmaHistoryOneFifth, yParentOneFifth] = OnePlusOneESOneFifth("Sphere", [], yStart, sigma, sigmaStop);
 
 set (0, "defaultaxesfontname", "Helvetica")
 set (0, "defaultaxesfontsize", 14)
 set (0, "defaulttextfontname", "Helvetica")
 set (0, "defaulttextfontsize", 14) 
 
-semilogy(result,'b-');
-ylabel("Fitness value");
-xlabel("Generation number");
-
-title("Exercise 3 - Fitness - 1+1 ES");
+semilogy(fitnessHistoryOneFifth,'b-');
 
 hold on
 
-maxGenCount = 7000;
+semilogy(sigmaHistoryOneFifth,'g-');
+
+hold on
+
+maxGenCount = 5000;
 result2 = OnePlusOneES(@Sphere, yStart, sigma, maxGenCount);
 semilogy(result2,'r-');
 
-legend("1+1 mit 1/5","1+1");
+legend("1+1 and 1/5","1+1", "Sigma");
+
+ylabel("Fitness / Sigma Value");
+xlabel("Generation Number");
+
+title("Exercise 3 - Fitness - 1+1 ES");
